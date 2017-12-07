2267a5208adde9258712ea98cbdfb399d913e461
Dan Versalov <dan.versalov@gmail.com>
2017-12-07 17:21:01 -0500

    New Website
    files uploaded

---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+$(window).on("load", function() {
+    $(".loadertext").fadeOut(1000);
+    $(".loader").fadeOut(2000);
+});
+
+$(document).ready(function(){
+    
+    $('.heading').hide().delay(800);
+    $('.hamspan').hide().delay(800);
+    $('.heading').fadeIn(1500);
+    $('.hamspan').delay(200).fadeIn(1500);
+
+    $(".navbarbtn").mouseenter(function(){
+
+        $("#btn-bar-2").css("transform", "translate(3px, 0px)");
+
+    });
+    
+    $(".navbarbtn").mouseleave(function(){
+        
+        $("#btn-bar-2").css("transform", "translate(0px, 0px)");
+        
+    });
+
+    $(".closebtn").mouseenter(function(){
+
+        $("#btn-bar-close2").css("transform", "translate(-3px, 0px)");
+
+    });
+    
+    $(".closebtn").mouseleave(function(){
+        
+        $("#btn-bar-close2").css("transform", "translate(0px, 0px)");
+        
+    });
+
+    $(".callvenus").mouseenter(function(){
+
+        $(".callvenus").html("716-831-8100");
+
+    });
+    
+    $(".callvenus").mouseleave(function(){
+        
+        $(".callvenus").html("Call");
+        
+    });
+
+});
+function openNav() {
+
+    document.getElementById("navbarid").style.width = "100%";
+
+}
+
+function closeNav() {
+    
+    document.getElementById("navbarid").style.width = "0%";
+    
+}
